0ce7ffa552d07fa6011e0a3badb59f99c1e9bfad
NV-Phong <0h9ng03@gmail.com>
2025-04-08 01:15:08 +0700

    CHORE - hot reload

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+declare const module: any;
 
 async function bootstrap() {
    const app = await NestFactory.create(ApiGatewayModule);
    await app.listen(process.env.port ?? 3000);
+
+   if (module.hot) {
+      module.hot.accept();
+      module.hot.dispose(() => app.close());
+   }
 }
 bootstrap();
