8e06867c93809f0f929fec2ecb8ba9e8aea6f1a8
NV-Phong <0h9ng03@gmail.com>
2025-04-09 13:41:53 +0700

    FEAT - protect routes

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MessagingModule } from '@app/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from '../config/jwt.strategy';
 
 @Module({
-   imports: [MessagingModule],
-   providers: [AuthService],
+   imports: [
+      MessagingModule,
+      JwtModule.registerAsync({
+         imports: [ConfigModule],
+         useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('auth.accessTokenSecret'),
+         }),
+         inject: [ConfigService],
+      }),
+   ],
+   providers: [AuthService, JwtStrategy],
    controllers: [AuthController],
 })
 export class AuthModule {}
